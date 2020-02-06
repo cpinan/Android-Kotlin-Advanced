3fa2fb5c689794aa0536afb1e57c12b763df344a
Carlos Pinan <Carlos.Pinan@fisglobal.com>
2020-02-06 17:10:54 -0500

    Added changes

--- JetpackComposer/app/src/main/java/com/carlospinan/jetpackcomposer/MainActivity.kt
+++ JetpackComposer/app/src/main/java/com/carlospinan/jetpackcomposer/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.ui.core.setContent
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutPadding
+import androidx.ui.material.Button
 import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.OutlinedButtonStyle
 import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.dp
@@ -20,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Greeting(name = "Android")
+                MyScreenContent()
             }
         }
     }
@@ -41,13 +43,29 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+fun MyScreenContent(
+        names: List<String> = listOf("Android", "there"),
+        counterState: CounterState = CounterState()
+) {
     Column {
         for (name in names) {
             Greeting(name = "$name")
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, height = 32.dp)
+        Counter(counterState)
     }
+}
+
+
+@Composable
+fun Counter(state: CounterState) {
+    Button(
+            text = "I've been clicked ${state.count} times",
+            onClick = { state.count++ }
+            //style = OutlinedButtonStyle()
+            //style = OutlinedButtonStyle()
+    )
 }
 
 @Preview
